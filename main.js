705d28612bc35bb18b9dc97f93b20eab42c44e07
Giovanni Perniola <gioperniola1999@gmail.com>
2024-09-30 16:50:45 +0200

    terminato selfwork variabili

---
+++ main.js
@@ -0,0 +1,15 @@
+let num1 = 5
+let num2 = 12
+
+console.log('Il valore della variabile num1 è ' + num1);
+
+console.log('Il valore della variabile num2 è ' + num2);
+
+let stringa = 'aulab'
+
+console.log('il valore della variabile stringa è ' + stringa);
+
+const PIGRECO = 24.1234
+
+console.log('Il valore di PIGRECO è ' + PIGRECO);
+
