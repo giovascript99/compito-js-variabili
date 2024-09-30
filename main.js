2a397745e4153fdcb7471601bae3ea3bc004a61a
Giovanni Perniola <gioperniola1999@gmail.com>
2024-09-30 17:02:54 +0200

    corretto selfwork

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-let num1 = 5
-let num2 = 12
+let num1 = 5;
+let num2 = 12;
 
-console.log('Il valore della variabile num1 è ' + num1);
+console.log('Il valore della variabile num1 è: ' + num1);
 
-console.log('Il valore della variabile num2 è ' + num2);
+console.log(`Il valore della variabile num2 è: ${num2}`);
 
-let stringa = 'aulab'
+let stringa = 'aulab';
+stringa = 'ciao a tutti!'
+console.log('il valore della variabile stringa è: ' + stringa);
 
-console.log('il valore della variabile stringa è ' + stringa);
-
-const PIGRECO = 24.1234
-
-console.log('Il valore di PIGRECO è ' + PIGRECO);
+const PIGRECO = 3.14;
+PIGRECO = 2.8
+console.log(`Il valore di PIGRECO è:  ${PIGRECO}`);
 
